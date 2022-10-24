27e04cc9cfbd47644075ba1ecd954c949de00a20
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-10-24 10:07:30 -0400

    Update File

---
+++ Link/Pages/Test/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Link.Data;
+using Link.Model;
+
+namespace Link.Pages.Test
+{
+    public class Index : PageModel
+    {
+        private readonly Link.Data.ApplicationDbContext _context;
+
+        public Index(Link.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Category != null)
+            {
+                Category = await _context.Category.ToListAsync();
+            }
+        }
+    }
+}
